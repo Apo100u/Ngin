32de65c04d6d146743118823882dc9920b8644e3
Apo100u <mailodspamu@o2.pl>
2022-06-30 23:10:20 +0200

    Remove -- leftover test code;

--- Ngin/Cards/CardSet.cs
+++ Ngin/Cards/CardSet.cs
@@ -4,19 +4,6 @@ namespace Ngin.Cards;
 
 public class CardSet
 {
-    public CardSet()
-    {
-        Card testCard1 = new Card("Jebnięcie");
-        Card testCard2 = new Card("Pizgnięcie");
-        Card testCard3 = new Card("Pierdolnięcie");
-        Card testCard4 = new Card("Zajebanie");
-        
-        cards.Add(testCard1);
-        cards.Add(testCard2);
-        cards.Add(testCard3);
-        cards.Add(testCard4);
-    }
-
     public int Count => cards.Count;
     
     private List<Card> cards = new();
