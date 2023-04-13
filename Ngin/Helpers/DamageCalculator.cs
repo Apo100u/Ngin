a83ffee42cbfbfc40901075e86df3a4abd7cf94f
Apo100u <mailodspamu@o2.pl>
2023-04-13 20:45:29 +0200

    Develop -- add helper calculator classes for damage and heal;

---
+++ Ngin/Helpers/DamageCalculator.cs
@@ -0,0 +1,30 @@
+﻿using Ngin.Cards.Effects;
+using Ngin.Characters;
+
+namespace Ngin.Helpers;
+
+public class DamageCalculator
+{
+    private readonly Character damagedCharacter;
+    private readonly Damage damage;
+
+    public DamageCalculator(Character damagedCharacter, Damage damage)
+    {
+        this.damagedCharacter = damagedCharacter;
+        this.damage = damage;
+    }
+
+    public int CalculateDamage()
+    {
+        int calculatedDamagePower = damage.Power > damagedCharacter.Health.Current
+            ? damagedCharacter.Health.Current
+            : damage.Power;
+
+        if (calculatedDamagePower < 0)
+        {
+            calculatedDamagePower = 0;
+        }
+        
+        return calculatedDamagePower;
+    }
+}
---
+++ Ngin/Helpers/HealCalculator.cs
@@ -0,0 +1,32 @@
+﻿using Ngin.Cards.Effects;
+using Ngin.Characters;
+
+namespace Ngin.Helpers;
+
+public class HealCalculator
+{
+    private readonly Character healedCharacter;
+    private readonly Heal heal;
+
+    public HealCalculator(Character healedCharacter, Heal heal)
+    {
+        this.healedCharacter = healedCharacter;
+        this.heal = heal;
+    }
+
+    public int CalculateHeal()
+    {
+        int missingHealth = healedCharacter.Health.Base - healedCharacter.Health.Current;
+        
+        int calculatedHealPower = heal.Power > missingHealth
+            ? missingHealth
+            : heal.Power;
+
+        if (calculatedHealPower < 0)
+        {
+            calculatedHealPower = 0;
+        }
+
+        return calculatedHealPower;
+    }
+}
